3536659ac54411db602a7e224e6412ce8c37b6ff
Ananth Arokiadoss A <ananth.a@tvsi.local>
2017-12-17 09:18:21 +0530

    grievance-knowmore

--- src/js/controllers/employer/jobs.js
+++ src/js/controllers/employer/jobs.js
@@ -1,11 +1,11 @@
 angular
 .module('app')
 .controller('jobscontrol', ['$scope', function ($scope) {
-    $scope.labels = ['100', '200', '300', '500', '600', '2000', '900'];
+    $scope.labels = ['0','100', '200', '300', '500', '600', '2000', '900'];
     $scope.series = ['Series A'];
   
     $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40]
+      [0,65, 59, 80, 81, 56, 55, 40]
     ];
     
     $scope.colors = [{
