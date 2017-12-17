8e7bd4e06e2df4c0ef350171482c3757ec56290e
Ananth Arokiadoss A <ananth.a@tvsi.local>
2017-12-17 15:01:52 +0530

    new

--- src/js/controllers/regulatory/access.js
+++ src/js/controllers/regulatory/access.js
@@ -1,83 +1,101 @@
-angular.module("app").controller('accessCtrl', ['$rootScope','$scope', '$state','$interval','$http','$timeout', function ($rootScope,$scope, $state,$interval,$http,$timeout) {
+angular.module("app").controller('accessCtrl', ['$rootScope', '$scope', '$state', '$interval', '$http', '$timeout', function ($rootScope, $scope, $state, $interval, $http, $timeout) {
     //alert('in');
     //        alert("addjobCtrl");
-    $scope.getddata=function(){debugger;$rootScope.ajaxloader=true;
+    $scope.getddata = function () {
+        debugger; $rootScope.ajaxloader = true;
         $http({
             method: "POST",
             url: "http://10.91.17.123:4000/auth/getnewapproval",
-            data:'',
-            header:{'content-type':'application/JSON'}
+            data: '',
+            header: { 'content-type': 'application/JSON' }
         }).then(function mySuccess(response) {
-            
-            $timeout(function() {
-                $rootScope.ajaxloader=false
-             }, 3000);
+
+            $timeout(function () {
+                $rootScope.ajaxloader = false
+            }, 3000);
             $scope.Accessdata = response['data']['ResponseObject'];
             debugger;
         }, function myError(response) {
-            $rootScope.ajaxloader=false;
+            $rootScope.ajaxloader = false;
         });
     }
-    $scope.empApprove=function(empdata){
+    $scope.empApprove = function (empdata) {
+        debugger;
+        $scope.token = localStorage.getItem('todo')
+        if ($scope.token) {
+            $scope.auth_token = $scope.token.auth_token;
+        }
+
+        $http({
+            method: "POST",
+            url: "http://10.91.17.123:4000/auth/empapproval/" + empdata._id,
+            data: '',
+            header: { 'content-type': 'application/JSON', 'auth_token': $scope.auth_token }
+        }).then(function mySuccess(response) {
+            $scope.Accessdata = response['data']['ResponseObject'];
+            debugger;
+        }, function myError(response) {
+
+        });
+    }
+    $scope.empReject = function (empdata) {
+        $scope.token = localStorage.getItem('todo')
+        if ($scope.token) {
+            $scope.auth_token = $scope.token.auth_token;
+        }
+        $http({
+            method: "POST",
+            url: "http://10.91.17.123:4000/auth/empreject/" + empdata._id,
+            data: '',
+            header: { 'content-type': 'application/JSON', 'auth_token': $scope.auth_token }
+        }).then(function mySuccess(response) {
+            $scope.Accessdata = response['data']['ResponseObject'];
+            debugger;
+        }, function myError(response) {
+
+        });
+    }
+    $scope.jobApprove = function (empdata) {
+        $scope.token = localStorage.getItem('todo')
+        if ($scope.token) {
+            $scope.auth_token = $scope.token.auth_token;
+        }
         debugger;
         $http({
             method: "POST",
-            url: "http://10.91.17.123:4000/auth/empapproval/"+empdata._id,
-            data:'',
-            header:{'content-type':'application/JSON'}
+            url: "http://10.91.17.123:4000/auth/empapproval/" + empdata._id,
+            data: '',
+            header: { 'content-type': 'application/JSON', 'auth_token': $scope.auth_token }
         }).then(function mySuccess(response) {
             $scope.Accessdata = response['data']['ResponseObject'];
             debugger;
         }, function myError(response) {
-          
+
         });
     }
-    $scope.empReject=function(empdata){
+    $scope.jobReject = function (empdata) {
+        $scope.token = localStorage.getItem('todo')
+        if ($scope.token) {
+            $scope.auth_token = $scope.token.auth_token;
+        }
         $http({
             method: "POST",
-            url: "http://10.91.17.123:4000/auth/empreject/"+empdata._id,
-            data:'',
-            header:{'content-type':'application/JSON'}
+            url: "http://10.91.17.123:4000/auth/empreject/" + empdata._id,
+            data: '',
+            header: { 'content-type': 'application/JSON', 'auth_token': $scope.auth_token }
         }).then(function mySuccess(response) {
             $scope.Accessdata = response['data']['ResponseObject'];
             debugger;
         }, function myError(response) {
-          
-        });
-    }
-    $scope.jobApprove=function(empdata){
-        debugger;
-        $http({
-            method: "POST",
-            url: "http://10.91.17.123:4000/auth/empapproval/"+empdata._id,
-            data:'',
-            header:{'content-type':'application/JSON'}
-        }).then(function mySuccess(response) {
-            $scope.Accessdata = response['data']['ResponseObject'];
-            debugger;
-        }, function myError(response) {
-          
-        });
-    }
-    $scope.jobReject=function(empdata){
-        $http({
-            method: "POST",
-            url: "http://10.91.17.123:4000/auth/empreject/"+empdata._id,
-            data:'',
-            header:{'content-type':'application/JSON'}
-        }).then(function mySuccess(response) {
-            $scope.Accessdata = response['data']['ResponseObject'];
-            debugger;
-        }, function myError(response) {
-          
+
         });
     }
     $scope.getddata();
-    $scope.stop = $interval(function() {
-        
+    $scope.stop = $interval(function () {
+
         $scope.getddata();
-      }, 15000);
-      $scope.$on("$destroy", function() {
+    }, 15000);
+    $scope.$on("$destroy", function () {
         $scope.stop();
-        });
+    });
 }]);
