da842690b5834a8c390e1ff870039d7de2cb6c41
Harish Selvaraj <harish.s@TVSi.Local>
2017-12-17 03:20:11 -0500

    add jobs api

---
+++ src/js/services/addJobs.js
@@ -0,0 +1,17 @@
+﻿angular.module("app").factory('addjobsService', [function () {
+
+  var service = {
+
+    jobPost: function (data,success, error) {
+      var headers = {
+        'Content-Type': 'application/json',
+        "auth_token":JSON.pare(localStorage.todos)
+      }
+      $http.post("http://10.91.17.123:4000/auth/jobpost", data, { headers: headers })
+        .then(success, error);
+
+    }
+  }
+  return service;
+
+}]);
